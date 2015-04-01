f32b94bc0826dfdef3268cf72d079f6fae7316e9
grupojf <fabricio.mascouto@gmail.com>
2015-04-01 09:38:22 -0300

    Create PrimeiroTeste.cpp

---
+++ PrimeiroTeste.cpp
@@ -0,0 +1,13 @@
+// 01/04/2015
+// FABRICIO DA SILVA MASCOUTO e HIASMIM HOREM
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Arquivo Fabricio e Hiasmim." << endl;
+    return 0;
+}
